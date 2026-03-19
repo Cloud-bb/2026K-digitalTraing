8b682dc2fe5a9ac8cfbda3b25cf069089800f8d4
cloudyB <157672950+Cloud-bb@users.noreply.github.com>
2026-03-19 16:59:28 +0900

    0319 java 반복문

---
+++ javaproject/src/p2026_03_19/Switch01.java
@@ -0,0 +1,28 @@
+package p2026_03_19;
+
+import java.util.Scanner;
+
+public class Switch01 {
+
+	public static void main(String[] args) {
+		System.out.println("0~100점 사이의 점수를 입력하시오");
+		Scanner sc = new Scanner(System.in);
+		
+		int s = sc.nextInt();
+		
+		
+		switch(s/10) {
+		case 10:
+		case 9:System.out.println("A학점");
+		break;
+		case 8:System.out.println("B학점");
+		break;
+		case 7:System.out.println("C학점");
+		break;
+		case 6:System.out.println("D학점");
+		break;
+		default:System.out.println("F학점");
+		}
+	}
+
+}
