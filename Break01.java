de45f2d68fa58bb9be59d706dae0f016e2b9ecbd
cloudyB <157672950+Cloud-bb@users.noreply.github.com>
2026-03-23 16:15:38 +0900

    0320 자바 보조제어문 break

---
+++ Break01.java
@@ -0,0 +1,17 @@
+package p2026_03_20;
+
+public class Break01 {
+
+	public static void main(String[] args) {
+		
+		// 무한루프 : for문 for(;;)
+		// break : 반복문을 빠져 나오는 역할
+		
+		for(int i=1;;i++) {
+			System.out.println(i+"무한출력");
+			if(i==100) break;
+		}
+		
+	}
+
+}
