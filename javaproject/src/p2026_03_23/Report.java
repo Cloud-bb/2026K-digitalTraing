606ff8c5f725b7c3d17ee3dc990ce7d2035b8232
cloudyB <157672950+Cloud-bb@users.noreply.github.com>
2026-03-23 16:43:55 +0900

    0320 과제 리뷰

---
+++ javaproject/src/p2026_03_23/Report.java
@@ -0,0 +1,20 @@
+package p2026_03_23;
+import java.util.Scanner;
+
+public class Report {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int n, f=1;
+		System.out.print("정수를 입력 하세요?");
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		
+		for(int i=n; i>=1; i--){
+			f = f * i;      // f *= i;
+		}
+		System.out.println(n+"!="+f);	
+	}
+
+}
