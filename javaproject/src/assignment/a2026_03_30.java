cfb241fb4f3b08b496d6609c9ab8e513c2e74401
Cloud-bb <taay_429@naver.com>
2026-03-30 17:29:02 +0900

    0330 과제 파일 업로드

---
+++ javaproject/src/assignment/a2026_03_30.java
@@ -0,0 +1,31 @@
+package assignment;
+
+import java.util.Arrays;
+
+public class a2026_03_30 {
+//1~45사이의 숫자를 6개 추출하는 프로그램을 작성하시오 (단, 중복된 숫자는 1번만 출력되도록한다) 
+	public static void main(String[] args) {
+		int[] lotto = new int[6];
+
+		for (int i = 0; i < lotto.length; i++) {
+			lotto[i] = (int) (Math.random() * 6) + 1;
+
+			// 중복검사
+			for (int j = 0; j < i; j++) {
+				if (lotto[i] == lotto[j])
+					i--;
+			}
+		}
+		for (int i = 0; i < lotto.length; i++) {
+			System.out.print(lotto[i] + "\t");
+		}
+		// 배열 정렬
+		Arrays.sort(lotto);
+		// 출력
+		System.out.println("정렬");
+		for (int i = 0; i < lotto.length; i++) {
+			System.out.print(lotto[i] + "\t");
+		}
+	}
+
+}
